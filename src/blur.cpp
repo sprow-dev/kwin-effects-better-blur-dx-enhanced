27d9bde601058c89301b8c2f58ac55330f0a1a21
Sprow-dev <sprowberry8@gmail.com>
2026-03-05 07:09:39 -0700

    Revert to old noise code
    because the new code is sht

--- src/blur.cpp
+++ src/blur.cpp
@@ -728,26 +728,21 @@ GLTexture *BlurEffect::ensureNoiseTexture()
         std::srand((uint)QTime::currentTime().msec());
 
         QImage noiseImage(QSize(256, 256), QImage::Format_Grayscale8);
-        
+
         for (int y = 0; y < noiseImage.height(); y++) {
             uint8_t *noiseImageLine = (uint8_t *)noiseImage.scanLine(y);
 
             for (int x = 0; x < noiseImage.width(); x++) {
-                int rnd = std::rand();
-                
                 // layer 1. intense (0-130, 10x banding)
-                int layer1 = (uint8_t)((static_cast<uint64_t>(rnd >> 10) * 10) % 131);
+                int layer1 = (uint8_t)((static_cast<uint64_t>(std::rand()) * 10) % 131);
 
                 // layer 2. soft (0-40, 3x banding)
-                int layer2 = (uint8_t)((static_cast<uint64_t>(rnd >> 3) * 3) % 41);
-                
-                // pre-calculate strength
-                float strength = m_noiseStrength / 10.0f;
-                
+                int layer2 = (uint8_t)((static_cast<uint64_t>(std::rand()) * 3) % 41);
+
                 // bend the lighting a bunch
                 int refraction = (layer1 * 1.96f) - (layer2 / 5.0f);
                 // blend them with subtle refraction
-                int blend = static_cast<int>(refraction * strength);
+                int blend = static_cast<int>(refraction * (m_noiseStrength / 10.0f));
                 
                 noiseImageLine[x] = (uint8_t)std::clamp(blend, 0, 255);
             }
@@ -767,6 +762,7 @@ GLTexture *BlurEffect::ensureNoiseTexture()
 
     return m_noisePass.noiseTexture.get();
 }
+
 
 void BlurEffect::blur(const RenderTarget &renderTarget, const RenderViewport &viewport, EffectWindow *w, int mask, const Region &deviceRegion, WindowPaintData &data)
 {
